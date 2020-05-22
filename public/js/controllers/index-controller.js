6119977c3632c9bbddcbea48d86deedddfdf4ba4
jvitorn <joaoeuno2011@gmail.com>
2020-05-22 16:18:17 -0300

    Client - Colocando animações

--- public/js/controllers/index-controller.js
+++ public/js/controllers/index-controller.js
@@ -1,6 +1,6 @@
 angular.module('ecothink').controller('IndexController',function($scope,$rootScope,$http){
     console.log('entrou no controller index');
 
-
+    $rootScope.isLogin = true;
     $rootScope.isUser =false;
 });
--- public/js/controllers/perfilController.js
+++ public/js/controllers/perfilController.js
@@ -1,4 +1,11 @@
 angular.module('ecothink').controller('PerfilController',function($scope,$rootScope,$http,$location){
     $rootScope.isLogin = false;
     $rootScope.isUser =true;
+
+
+    //sair
+    $scope.logout = ()=>{
+       localStorage.clear();
+       $location.path('home/') 
+    }
 });
