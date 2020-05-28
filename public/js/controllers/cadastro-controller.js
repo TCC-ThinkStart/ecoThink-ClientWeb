fb673749f5ece5a72196304c7d6f2257ec9cd715
lucasleaosobral <lucasviniciusleao@hotmail.com>
2020-05-28 16:09:14 -0300

    adicionando validação e enviando dados do cadastro

--- public/js/controllers/cadastro-controller.js
+++ public/js/controllers/cadastro-controller.js
@@ -1,26 +1,29 @@
-angular.module('ecothink').controller('CadastroController',function($scope,$http,$location,$window){
+angular.module('ecothink').controller('CadastroController', function ($scope, $http, $location, $window) {
     $scope.usuario = {};
     $scope.mensagem = '';
-    console.log('entrou no controller de cadastro');
-    console.log($scope.usuario);
-    $scope.submeter = function(){
-        //se existir um formulario valido
-        if($scope.formulario.$valid){
-            $http.post('/usuario',$scope.usuario,{headers:{'adress':'e8da82b2'}})
-            .success(function(){
-                console.log($scope.usuario.email);
-                console.log($scope.usuario);
-                $scope.usuario = {};
-                $scope.mensagem ='usuario cadastrado com sucesso';
-                $window.location.href = '/';
-                //futuramente redirecionar o usuario para se logar
-            })
-            .error(function(error){
-                console.error(error);
-                $scope.mensagem = 'Não Foi Possivel se cadastrar,favor tente novamente';
-            });
-        
+
+    $scope.submitar = (usuario) => {
+        if (usuario.nome && usuario.email && usuario.senha && usuario.confirmar) {
+            if (usuario.senha === usuario.confirmar) {
+                $scope.isValid = true;
+
+                return false
+            } else {
+                return true
+            }
+
+        } else {
+
+            return true
         }
 
+    }
+    $scope.submeter = (usuario) => {
+        console.log(usuario)
+        $http.post('http://localhost:4200/login', usuario)
+            .then(results => { console.log(results.data) })
+            .catch(error => {
+                console.log(error)
+            })
     }
 });
--- public/js/controllers/cadastroOngController.js
+++ public/js/controllers/cadastroOngController.js
@@ -1,43 +1,34 @@
-angular.module('ecothink').controller('CadastroOngController',function($scope,$http,$location,$window,$rootScope){
-   
+angular.module('ecothink').controller('CadastroOngController', function ($scope, $http, $location, $window, $rootScope) {
+
 
     $rootScope.isLogin = false;
-    $rootScope.isDark = true; 
+    $rootScope.isDark = true;
 
-    $rootScope.isUser =false;
+    $rootScope.isUser = false;
 
 
-    $scope.submitar = (usuario)=>{
-        if(usuario.nome && usuario.email && usuario.senha && usuario.confirmar && usuario.cnpj){
-            if(usuario.senha === usuario.confirmar){
+    $scope.submitar = (usuario) => {
+        if (usuario.nome && usuario.email && usuario.senha && usuario.confirmar && usuario.cnpj) {
+            if (usuario.senha === usuario.confirmar) {
                 $scope.isValid = true;
+
                 return false
-            }else{
+            } else {
                 return true
             }
-            
-        }else{
-            
+
+        } else {
+
             return true
         }
-        
+
     }
-    $scope.submeter = (usuario)=>{
-        $http.post('http://localhost:4200/login',usuario)
-        .then(results=>{
-            if(results.data.auth == true){
-                window.location.href  = '/user/perfil';
-            
-            }else if(results.data.auth == false) {
-                window.location.href  = '/home/cadastro';
-               
-            
-            }else{
-                console.log('nao entrou em nenhum if')
-            }
-        })
-        .catch(error=>{
-            console.log(error)
-        })
+    $scope.submeter = (usuario) => {
+        console.log(usuario)
+        $http.post('http://localhost:4200/login', usuario)
+            .then(results => { console.log(results.data) })
+            .catch(error => {
+                console.log(error)
+            })
     }
 });
