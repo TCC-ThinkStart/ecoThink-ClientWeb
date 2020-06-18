c875ad1966188ce2401f9ada804b78bc4ab33a62
jvitorn <joaoeuno2011@gmail.com>
2020-06-18 13:03:01 -0300

    adicionando mais detalhe no editarPerfil

--- public/js/controllers/perfilController.js
+++ public/js/controllers/perfilController.js
@@ -1,11 +1,11 @@
-angular.module('ecothink').controller('PerfilController',function($scope,$rootScope,$http,$location){
+angular.module('ecothink').controller('PerfilController', function ($scope, $rootScope, $http, $location) {
     $rootScope.isLogin = false;
-    $rootScope.isUser =true;
+    $rootScope.isUser = true;
     $rootScope.isDark = false;
 
     //sair
-    $scope.logout = ()=>{
-       localStorage.clear();
-       $location.path('home/') 
+    $scope.logout = () => {
+        localStorage.clear();
+        $location.path('home')
     }
 });
