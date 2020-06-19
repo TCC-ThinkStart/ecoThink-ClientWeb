01f82395f02e57b526e6fd0d4055761377d7e81b
jvitorn <joaoeuno2011@gmail.com>
2020-06-19 11:27:26 -0300

    adicionando validação na foto de edicao

--- public/js/controllers/editarPerfilController.js
+++ public/js/controllers/editarPerfilController.js
@@ -8,4 +8,24 @@ angular.module('ecothink').controller('EditarPerfilController', function ($scope
             return true
         }
     }
+
+    $scope.SelectFile = (e) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const valida = e.target.result.indexOf('data:image/')
+
+            if (valida == 0) {
+                //encriptada
+                $scope.PreviewImage = e.target.result;
+                $scope.$apply();
+            } else {
+                alert('favor insira uma imagem')
+            }
+
+
+        };
+
+        reader.readAsDataURL(e.target.files[0]);
+        console.log('entrou')
+    }
 });
