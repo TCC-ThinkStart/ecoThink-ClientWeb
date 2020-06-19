3e07274b647d6a8cd0292fe6c40a46c3be6edd0b
jvitorn <joaoeuno2011@gmail.com>
2020-06-19 10:22:20 -0300

    adicionando mascara de telefone

--- public/js/controllers/editarPerfilController.js
+++ public/js/controllers/editarPerfilController.js
@@ -1,3 +1,18 @@
 angular.module('ecothink').controller('EditarPerfilController', function ($scope, $http) {
+    $scope.submitar = (usuario) => {
+        if (usuario.nome && usuario.email && usuario.senha && usuario.confirmar && usuario.cnpj) {
+            if (usuario.senha === usuario.confirmar) {
+                $scope.isValid = true;
 
+                return false
+            } else {
+                return true
+            }
+
+        } else {
+
+            return true
+        }
+
+    }
 });
