0efc818dfb496a91e512a2ae0affdc7585875f6f
jvitorn <joaoeuno2011@gmail.com>
2020-06-25 15:33:04 -0300

    adicionando foto em eventos aleatorias

--- public/js/controllers/descobrirController.js
+++ public/js/controllers/descobrirController.js
@@ -1,10 +1,25 @@
 angular.module('ecothink').controller('DescobrirController', function ($scope, $http, recursoLogin, recursoEvento, $rootScope) {
     recursoLogin.verify;
-    
-    $http.get($rootScope.api + '/evento')
+
+    $http.get($rootScope.api + '/evento?random')
         .then(response => {
             $scope.aleatorio = response.data;
             console.log(response.data)
+            //foto desse evento
+            for (let c = 0; c <= response.data.length; c++) {
+                $http.get($rootScope.api + '/foto/evento/' + response.data[c].codigo)
+                    .then(results => {
+                        if (results.data.length == 1) {
+                            console.log(results.data)
+                            $scope.aleatorio[c].img = $rootScope.api + '/' + results.data[0].url;
+                        }
+
+                        //evento tem foto
+
+                    })
+                    .catch(error => console.log(error))
+
+            }
         })
         .catch(error => console.warn(error))
 
--- public/js/controllers/usuarioController.js
+++ public/js/controllers/usuarioController.js
@@ -18,9 +18,25 @@ angular.module('ecothink').controller('UsuarioController', function ($scope, $ro
             //visualizando os eventos desse usuario
             $http.get($rootScope.api + '/evento/usuario/' + $routeParams.usuarioId)
                 .then(results => {
-                    console.log(results.data)
-                    const titulos = results.data.map((eventos) => { return eventos.nome })
-                    $scope.titulos = titulos
+
+
+                    $scope.titulos = results.data
+                    //foto desse evento
+                    for (let c = 0; c <= results.data.length; c++) {
+                        $http.get($rootScope.api + '/foto/evento/' + results.data[c]['codigo'])
+                            .then(results => {
+                                if (results.data.length == 1) {
+                                    $scope.titulos[c].img = $rootScope.api + '/' + results.data[0].url
+                                    console.log($scope.titulos)
+                                }
+
+                                //evento tem foto
+
+                            })
+                            .catch(error => console.log(error))
+                    }
+
+
                 })
         })
     }
