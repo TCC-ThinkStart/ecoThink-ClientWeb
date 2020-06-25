50ff1aafaa931b01c01c8434277f3de645eb0b44
jvitorn <joaoeuno2011@gmail.com>
2020-06-25 19:43:26 -0300

    adicionando api de municipios para mostrar em controller de pesquisa

--- public/js/controllers/pesquisarController.js
+++ public/js/controllers/pesquisarController.js
@@ -1,5 +1,8 @@
-angular.module('ecothink').controller('PesquisarController', function ($rootScope, $scope, $http, $location, $window) {
+angular.module('ecothink').controller('PesquisarController', function ($rootScope, $scope, $http, $location, $window, recursoCity) {
     $rootScope.isLogin = false;
     $rootScope.isUser = true;
     $rootScope.isDark = false;
+
+
+    recursoCity.query((results) => $scope.municipios = results)
 });
