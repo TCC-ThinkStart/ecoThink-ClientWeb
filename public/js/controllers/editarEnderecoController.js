896daa1adb00e912e2f3178c39409ac0fbd6e76a
jvitorn <joaoeuno2011@gmail.com>
2020-06-26 12:50:40 -0300

    adicionando teste de editar endereco

--- public/js/controllers/editarEnderecoController.js
+++ public/js/controllers/editarEnderecoController.js
@@ -1,5 +1,8 @@
 angular.module('ecothink').controller('EditarEnderecoController', function ($scope, $http, $rootScope, recursoLogin) {
     $rootScope.isLogin = false;
 
+    $scope.enviar = (usuario) => {
 
+        console.log(usuario)
+    }
 });
