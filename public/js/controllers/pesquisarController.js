3ad8c706eab3aed68b352abdddcab2f494b7fa0f
jvitorn <joaoeuno2011@gmail.com>
2020-06-29 09:17:57 -0300

    pesquisa por nome - funcional

--- public/js/controllers/pesquisarController.js
+++ public/js/controllers/pesquisarController.js
@@ -2,7 +2,19 @@ angular.module('ecothink').controller('PesquisarController', function ($rootScop
     $rootScope.isLogin = false;
     $rootScope.isUser = true;
     $rootScope.isDark = false;
-
+    $scope.results = {}
 
     recursoCity.query((results) => $scope.municipios = results)
+
+
+    $scope.pesquisaNome = (pesquisa) => {
+        $http.get($rootScope.api + '/evento/pesquisa/' + pesquisa.nome)
+            .then(results => {
+                $scope.resultados = results.data
+                console.log(results)
+            })
+            .catch(error => console.warn(error))
+    }
+
+
 });
