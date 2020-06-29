8fb27ce6f0cb5c8c6dd1f2d451b8c69624c9ac2c
jvitorn <joaoeuno2011@gmail.com>
2020-06-29 15:21:34 -0300

    adicionando badge de ação social

--- public/js/controllers/pesquisarController.js
+++ public/js/controllers/pesquisarController.js
@@ -37,7 +37,7 @@ angular.module('ecothink').controller('PesquisarController', function ($rootScop
     }
 
     $scope.pesquisarCity = (pesquisar) => {
-        console.log(pesquisar.cidade)
+
 
         $http.get($rootScope.api + '/evento/cidade/' + pesquisar.cidade.codigo)
             .then(results => {
@@ -66,4 +66,32 @@ angular.module('ecothink').controller('PesquisarController', function ($rootScop
             .catch(error => console.warn(error))
     }
 
+    $scope.pesquisarChave = (pesquisar) => {
+
+        $http.get($rootScope.api + '/evento/palavrachave/' + pesquisar.palavrachave.codigo)
+            .then(results => {
+                $scope.resultados = results.data
+                console.log(results.data)
+                for (let c = 0; c <= results.data.length; c++) {
+
+                    $http.get($rootScope.api + '/foto/evento/' + results.data[c].codigo)
+                        .then(imagens => {
+                            //evento tem foto
+                            if (imagens.data.length == 1) {
+                                //https://s3.amazonaws.com/mapa-da-obra-producao/wp-content/uploads/2019/10/shutterstock_1110036392.png
+                                $scope.resultados[c].imagem = $rootScope.api + '/' + imagens.data[0].url;
+
+                            }
+                            //evento nao tem foto 
+                            else {
+
+                            }
+                        })
+                        .catch(error => console.log(error))
+                }
+
+                console.log(results)
+            })
+            .catch(error => console.warn(error))
+    }
 });
