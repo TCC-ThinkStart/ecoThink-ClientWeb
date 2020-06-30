4b3ae065a5f6b6369f3344fb7f23a6af5bb84dcb
jvitorn <joaoeuno2011@gmail.com>
2020-06-30 12:18:40 -0300

    adicionando teste de segunda versao de criar evento Part I

--- public/js/main.js
+++ public/js/main.js
@@ -50,7 +50,7 @@ angular.module('ecothink', ['minhasDiretivas', 'ngRoute', 'meusServicos', 'ui.ma
             templateUrl: 'partials/criarEvento.html',
             controller: 'CriarEventosController'
         });
-        $routeProvider.when('/user/criarevento', {
+        $routeProvider.when('/user/criarevento2', {
             templateUrl: 'partials/criarEvento2.html',
             controller: 'CriarEventos2Controller'
         });
