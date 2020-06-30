a6c51a3dd6fc1bb039b6a77cff80672f728ac184
jvitorn <joaoeuno2011@gmail.com>
2020-06-30 16:52:53 -0300

    adicionando objeto de foto na nova requisição de evento

--- public/js/controllers/criarEvento2Controller.js
+++ public/js/controllers/criarEvento2Controller.js
@@ -10,7 +10,25 @@ angular.module('ecothink').controller('CriarEventos2Controller', function ($scop
 
     console.log($scope.evento)
 
+    $scope.SelectFile = (e) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const valida = e.target.result.indexOf('data:image/')
 
+            if (valida == 0) {
+                //encriptada
+                $scope.PreviewImage = e.target.result;
+                $scope.$apply();
+            } else {
+                alert('favor insira uma imagem')
+            }
+
+
+        };
+
+        reader.readAsDataURL(e.target.files[0]);
+
+    };
 
     $scope.enviar = (events) => {
 
@@ -26,12 +44,14 @@ angular.module('ecothink').controller('CriarEventos2Controller', function ($scop
 
     }
     $scope.inserir = () => {
-
+        //capturando todos os dados do sessionStorage
         $scope.eventoEnviar = $scope.evento[0]
         $scope.eventoEnviar.cep = $scope.evento[1].cep
         $scope.eventoEnviar.logradouro = $scope.evento[1].logradouro
         $scope.eventoEnviar.bairro = $scope.evento[1].bairro
         $scope.eventoEnviar.numero = $scope.evento[1].numero
+
+        $scope.eventoEnviar.foto = { base64: $scope.PreviewImage }
         console.log($scope.eventoEnviar)
 
 
