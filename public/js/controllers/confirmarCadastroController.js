b6e83043f26cc645acebea1aae9f1329cdadb02d
jvitorn <joaoeuno2011@gmail.com>
2020-07-03 12:02:52 -0300

    arrumando requisição de confirmação de cadastro

--- public/js/controllers/confirmarCadastroController.js
+++ public/js/controllers/confirmarCadastroController.js
@@ -1,19 +1,38 @@
 angular.module('ecothink').controller('ConfirmarCadastroController', function ($scope, $rootScope, $http, $location) {
-    alert('kdfmdkmfk')
+
     $rootScope.isLogin = true;
     $rootScope.isUser = false;
 
-    $scope.submitar = (usuario) => {
-        if (usuario.token) {
-            $scope.isValid = true;
-            return false
-        } else {
-            return true
-        }
 
-    }
     $scope.submeter = (usuario) => {
-
         console.log(usuario)
+        $scope.token = 'Bearer ' + usuario.token
+        console.log($scope.token)
+
+
+        $http.put($rootScope.api + '/usuario/confirmacao/confirmar', {
+            headers: { 'Authorization': 'Bearer ' + $scope.token }
+        }).then(results => {
+            console.log(results)
+            const mensagem = results.data.success;
+            console.log(mensagem)
+            Swal.fire({
+                icon: 'success',
+                title: 'Usuario',
+                text: mensagem,
+                footer: "<a href='home/login'>Agora faça o Login!</a>"
+            })
+        })
+            .catch(error => {
+                console.error(error)
+                const msg = error.data.error
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Erro',
+                    text: msg + ',Favor tente Novamente!',
+                })
+            })
+
+
     }
 });
